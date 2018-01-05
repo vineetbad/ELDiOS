eca7e993127107cb1511e731959ab7d2451b4358
vineetbad <vineetbad@gmail.com>
2018-01-05 12:27:59 -0800

    Starting PopOverViewController

--- ELDHOS/Controller/HoursOfServiceViewController.swift
+++ ELDHOS/Controller/HoursOfServiceViewController.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 
-class HoursOfServiceViewController: UIViewController {
+class HoursOfServiceViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
 
+    @IBOutlet var drivingOptions: UIPickerView!
+    @IBOutlet var timerDriving: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //TODO : load all the information from the database based on the username and password from the last page
         // Do any additional setup after loading the view.
+        self.drivingOptions.delegate = self
+        self.drivingOptions.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +29,36 @@ class HoursOfServiceViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    
+    //------THIS IS THE PICKER STUFF ------//
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return DrivingOptions().allDrivingOptions.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return DrivingOptions().allDrivingOptions[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        popNewEvent(drivingOption : DrivingOptions().allDrivingOptions[row])
+    }
+    
+    
+    //------ END ------//
+    
+    
+    //---- DO THE NEW EVENT WHENEVER SOMETHING IS PICKED ------//
+    
+    func popNewEvent(drivingOption : String){
+        //TODO Over here I want to present popover presentation of the event creater
+        
+    }
+    
+    
+    
     /*
     // MARK: - Navigation
 
---
+++ ELDHOS/Model/DrivingOptions.swift
@@ -0,0 +1,12 @@
+//
+//  DrivingOptions.swift
+//  ELDHOS
+//
+//  Created by Vineet Baid on 04/01/18.
+//  Copyright © 2018 Vineet Baid. All rights reserved.
+//
+
+import Foundation
+struct DrivingOptions {
+    let allDrivingOptions = ["OffDuty", "Sleeper", "On Duty", "Driving"]
+}
